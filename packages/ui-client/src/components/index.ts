1def369f5f5e56b4081c8953600848c13d0b5e95
Douglas Fabris <devfabris@gmail.com>
2025-10-23 10:35:35 -0300

    chore: use components from `ui-client`

--- packages/ui-client/src/components/index.ts
+++ packages/ui-client/src/components/index.ts
@@ -7,7 +7,6 @@ export * from './PasswordVerifier/PasswordVerifier';
 export { default as TextSeparator } from './TextSeparator';
 export * from './TooltipComponent';
 export * as UserStatus from './UserStatus';
-export * from './Header';
 export * from './HeaderV2';
 export * from './MultiSelectCustom/MultiSelectCustom';
 export * from './FeaturePreview';
